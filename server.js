1f034caa6962d8b220d4180340598d1c39318fed
Invincible <87626336+vbmisra@users.noreply.github.com>
2022-01-20 18:35:40 -0800

    edited server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // import express and routes for pages and notes
+const { urlencoded } = require('express');
 const express = require('express');
 const noteRoutes = require('./routes/noteRoutes')
 const pageRoutes = require('./routes/pageRoutes')
@@ -8,8 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use('/api', noteRoutes)
+app.use('/', pageRoutes)
 
-
+//start server
 app.listen(PORT, () => 
     console.log(`App listening at ${PORT}`)
 )
